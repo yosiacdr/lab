80d7ef6e860488e6e12aac7611af7c9b0feec596
yosiacdr <75170837+yosiacdr@users.noreply.github.com>
2020-11-28 17:02:08 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace lab4
+{
+    class ArrayExample
+    {
+        static void Main()
+        {
+            char[] letters = { 'E', 'g', 'o', 'r', ' ', 'K', 'o', 'n', 's', 't', 'a', 'n', 't', 'i', 'n', 'o', 'v', };
+            string name = "";
+            int[] a = new int[19];
+            for (int i = 0; i < letters.Length; i++)
+            {
+                name += letters[i];
+                a[i] = i + 1;
+                SendMessage(name, a[i]);
+            }
+            Console.ReadKey();
+        }
+        static void SendMessage(string name, int msg)
+        {
+            Console.WriteLine("Hello, " + name + "! Count to " + msg);
+        }
+    }
+}
